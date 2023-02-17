1502be483e49575882f66b282ea3ae8ba1d52772
gabmart1995 <gabmart1995@gmail.com>
2023-02-16 20:22:17 -0400

    wip: actualizacion de modulo 05 y 06

---
+++ 06-twittor/js/sw-utils.js
@@ -0,0 +1,24 @@
+/** 
+ * Guarda en cache dinamico 
+ * @param {Response} response
+ * @param {Request} request
+ * @param {string} dynamicCache nombre del cache dinamico
+ * @returns {Promise<Cache>|Response}
+ * */
+function updateDynamicCache( dynamicCache, request, response ) {
+
+    if ( response.ok ) {
+        
+        return caches.open( dynamicCache )
+            .then( cache => {
+
+                // actualiza el cache
+                cache.put( request, response.clone() );
+                
+                // devuelve un clon de la respuesta
+                return response.clone();
+            });
+    }
+
+    return response;
+}
---
+++ 06-twittor/sw.js
@@ -0,0 +1,79 @@
+// imports
+importScripts('js/sw-utils.js');
+
+const STATIC_CACHE = 'static_v2';
+const DYNAMIC_CACHE = 'dynamic_v1';
+const INMUTABLE_CACHE = 'inmutable_v1';
+
+const APP_SHELL = [
+    '/',
+    'index.html',
+    'css/style.css',
+    'img/favicon.ico',
+    'img/avatars/hulk.jpg',
+    'img/avatars/ironman.jpg',
+    'img/avatars/spiderman.jpg',
+    'img/avatars/thor.jpg',
+    'img/avatars/wolverine.jpg',
+    'js/app.js',
+    'js/sw-utils.js',
+];
+
+const APP_SHELL_INMUTABLE = [
+    'https://fonts.googleapis.com/css?family=Quicksand:300,400',
+    'https://fonts.googleapis.com/css?family=Lato:400,300',
+    'https://use.fontawesome.com/releases/v5.3.1/css/all.css',
+    'css/animate.css',
+    'js/libs/jquery.js',
+];
+
+
+// instalation
+self.addEventListener('install', event => {
+    
+    const cacheStatic = caches.open( STATIC_CACHE )
+        .then( cache => cache.addAll( APP_SHELL ));
+
+    
+    const cacheInmutable = caches.open( INMUTABLE_CACHE )
+        .then( cache => cache.addAll( APP_SHELL_INMUTABLE ));
+    
+    // esperamos el inicio de instalacion
+    event.waitUntil( Promise.all([ cacheStatic, cacheInmutable ]) );
+});
+
+// activate
+self.addEventListener('activate', event => {
+    
+    const response = caches.keys()
+        .then( keys => {
+
+            keys.forEach( key => {
+            
+                if ( key !== STATIC_CACHE && key.includes('static') ) {
+                    return caches.delete( key );
+                }
+            });
+        });
+
+    event.waitUntil( response );
+});
+
+self.addEventListener('fetch', event => {
+
+    const promiseResponse = caches.match( event.request )
+        .then( response => {
+            
+            if ( response ) {
+                return response;
+            }
+
+            // fetch al recurso nuevo inicializacion de cache dinamico
+            return fetch( event.request )
+                .then( newResponse => {
+                    return updateDynamicCache( DYNAMIC_CACHE, event.request, newResponse );
+                });
+        });
+
+    event.respondWith( promiseResponse );
+});
